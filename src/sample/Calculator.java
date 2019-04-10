084ebc2e2c9acbd336dbce450c6c95a6764c49b9
Jean-François <jean-francois.durand@epitech.eu>
2019-04-10 18:05:35 +0200

    BASE_TEST

---
+++ src/sample/Calculator.java
@@ -0,0 +1,22 @@
+package sample;
+
+public class Calculator {
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int substract(int x, int y) {
+        return x - y;
+    }
+
+    public int multiply(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        if (y != 0)
+            return x / y;
+        else
+            return 0;
+    }
+}
